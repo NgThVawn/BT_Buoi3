82d8ed7604cc2d4815ac77b682136fa5ab1db565
NgThVawn <nguyenthanhvane@gmail.com>
2025-09-22 09:05:19 +0700

    Thay đổi label tên đăng nhập

--- BT_Buoi3/BT_Buoi3/Form1.Designer.cs
+++ BT_Buoi3/BT_Buoi3/Form1.Designer.cs
@@ -48,9 +48,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(253, 105);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 20);
+            this.label1.Size = new System.Drawing.Size(116, 20);
             this.label1.TabIndex = 1;
-            this.label1.Text = "UserName";
+            this.label1.Text = "Tên đăng nhập";
             // 
             // label2
             // 
